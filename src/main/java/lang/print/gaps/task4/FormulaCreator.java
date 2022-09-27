8fb65c26c857f9f952e9a6993eab95ed08fbb70e
Phobos027 <shkuro.stepan@gmail.com>
2022-09-28 00:54:19 +0300

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 1;
+        double b = 2;
+        double formula = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
     }
 }
